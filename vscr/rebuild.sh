c9becbed3796792598c26ef234f8d7188fb884ab
Andrejs <andrew.k2@gmail.com>
2019-04-11 07:43:03 +0300

    Update rebuild.sh

--- vscr/rebuild.sh
+++ vscr/rebuild.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 cd /srv/app
-sudo git pull 2>&1 | tee /tmp/git.log
 
-answer=`cat /tmp/git.log`
-
-if [ $answer != "Already up to date." ]; then
-	echo New updates availiable
-	echo Building Angular example app
-	echo Please w8 up to 5 min...
+if [[ `git status --porcelain` ]]; then
+	echo "New updates availiable"
+	echo "Building Angular example app"
+	echo "Please w8 up to 5 min..."
 	sudo npm run build:prod:en
 
-	echo Restarting nginx...
+	echo "Restarting nginx..."
 	sudo systemctl restart nginx
 else
-	echo Already up to date
-	echo Nothing to do...
+	echo "Cheked for updates"
+	echo "Nothing to do..."
 fi
-
-sudo rm /tmp/git.log
