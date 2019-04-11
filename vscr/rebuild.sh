48d13033be889d2cdd4028c775518dc32aea1c0b
Andrejs <andrew.k2@gmail.com>
2019-04-11 09:52:40 +0300

    Update rebuild.sh

--- vscr/rebuild.sh
+++ vscr/rebuild.sh
@@ -5,7 +5,7 @@ cd /srv/app
 answer="Your branch is up to date with 'origin/master'."
 
 if [[ `git status | grep "is up to date"` == $answer ]]; then
-	echo "Cheked for updates"
+	echo "Checked for updates"
 	echo "Nothing to do..."
 else
 	echo "New updates availiable"
