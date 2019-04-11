6cc39327129e29f54e80a8145a0e272e96db158a
Andrejs <andrew.k2@gmail.com>
2019-04-11 09:07:40 +0300

    Update rebuild.sh

--- vscr/rebuild.sh
+++ vscr/rebuild.sh
@@ -4,10 +4,11 @@ cd /srv/app
 
 if [[ `git status --porcelain` ]]; then
 	echo "New updates availiable"
+	echo "Get updates"
+	sudo git pull
 	echo "Building Angular example app"
 	echo "Please w8 up to 5 min..."
 	sudo npm run build:prod:en
-
 	echo "Restarting nginx..."
 	sudo systemctl restart nginx
 else
